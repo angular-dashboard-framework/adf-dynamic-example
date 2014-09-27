35779f689695c62a9bca455bdcd381facf2ac4c3
Sebastian Sdorra <s.sdorra@gmail.com>
2014-09-27 22:35:11 +0200

    initial import

---
+++ public/scripts/app.js
@@ -0,0 +1,156 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2014, Sebastian Sdorra
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+'use strict';
+
+angular.module('adfDynamicSample', [
+    'adf', 'ngRoute', 'structures',
+    'sample.widgets.news', 'sample.widgets.weather',
+    'sample.widgets.linklist', 'sample.widgets.randommsg'
+  ])
+  .config(function($routeProvider){
+    $routeProvider
+      .when('/boards', {
+        templateUrl: 'partials/default.html'
+      })
+      .when('/boards/:id', {
+        controller: 'dashboardCtrl',
+        controllerAs: 'dashboard',
+        templateUrl: 'partials/dashboard.html',
+        resolve: {
+          data: function($route, storeService){
+            return storeService.get($route.current.params.id);
+          }
+        }
+      })
+      .otherwise({
+        redirectTo: '/boards'
+      });
+  })
+  .service('storeService', function($http, $q){
+    return {
+      getAll: function(){
+        var deferred = $q.defer();
+        $http.get('/v1/store')
+          .success(function(data){
+            deferred.resolve(data.dashboards);
+          })
+          .error(function(){
+            deferred.reject();
+          });
+        return deferred.promise;
+      },
+      get: function(id){
+        var deferred = $q.defer();
+        $http.get('/v1/store/' + id)
+          .success(function(data){
+            deferred.resolve(data);
+          })
+          .error(function(){
+            deferred.reject();
+          });
+        return deferred.promise;
+      },
+      set: function(id, data){
+        var deferred = $q.defer();
+        $http.post('/v1/store/' + id, data)
+          .success(function(data){
+            deferred.resolve();
+          })
+          .error(function(){
+            deferred.reject();
+          });
+        return deferred.promise;
+      },
+      delete: function(id){
+        var deferred = $q.defer();
+        $http.delete('/v1/store/' + id)
+          .success(function(data){
+            deferred.resolve(data);
+          })
+          .error(function(){
+            deferred.reject();
+          });
+        return deferred.promise;
+      }
+    };
+  })
+  .controller('navigationCtrl', function($scope, $q, $location, storeService){
+    var nav = this;
+    nav.navCollapsed = true;
+
+    this.toggleNav = function(){
+      nav.navCollapsed = ! nav.navCollapsed;
+    };
+
+    this.navClass = function(page) {
+      var currentRoute = $location.path().substring(1);
+      return page === currentRoute || new RegExp(page).test(currentRoute) ? 'active' : '';
+    };
+
+    this.create = function(){
+      var id = '_' + new Date().getTime();
+      var q = storeService.set(id, {
+        "title": "New Sample",
+        "structure": "4-8",
+        "rows": [{
+          "columns": [{
+            "styleClass": "col-md-4",
+            "widgets": []
+          },{
+            "styleClass": "col-md-8",
+            "widgets": []
+          }]
+        }]
+      });
+
+      $q.all([q, storeService.getAll()]).then(function(values){
+        nav.items = values[1];
+      });
+    };
+
+    storeService.getAll().then(function(data){
+      nav.items = data;
+    });
+
+    $scope.$on('navChanged', function(){
+      storeService.getAll().then(function(data){
+        nav.items = data;
+      });
+    });
+  })
+  .controller('dashboardCtrl', function($location, $rootScope, $scope, $routeParams, storeService, data){
+    this.name = $routeParams.id;
+    this.model = data;
+
+    this.delete = function(id){
+      storeService.delete(id);
+      $location.path('/');
+      $rootScope.$broadcast('navChanged');
+    };
+
+    $scope.$on('adfDashboardChanged', function(event, name, model) {
+      storeService.set(name, model);
+    });
+  });
---
+++ server.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var compression = require('compression');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var app = express();
+
+var encoding = 'utf8';
+var storeDir = __dirname + '/store/';
+
+app.use(compression());
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+
+app.get('/v1/store', function(req, res){
+  fs.readdir(storeDir, function(err, files){
+    var boards = [];
+    files.forEach(function(file){
+      var json = JSON.parse(fs.readFileSync(storeDir + file, encoding));
+      boards.push({
+        id: file.replace('.json', ''),
+        title: json.title
+      });
+    });
+    // send response
+    res.json({
+      dashboards: boards
+    });
+  });
+});
+
+app.get('/v1/store/:id', function(req, res){
+  fs.readFile(storeDir + req.params.id + '.json', encoding, function(err, data){
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(data);
+  });
+});
+
+app.post('/v1/store/:id', function(req, res){
+  fs.writeFile(
+    storeDir + req.params.id + '.json',
+    JSON.stringify(req.body, undefined, 2)
+  );
+  res.status(204).end();
+});
+
+app.delete('/v1/store/:id', function(req, res){
+  fs.unlinkSync(storeDir + req.params.id + '.json');
+  res.status(204).end();
+});
+
+var server = app.listen(3000, function() {
+    console.log('Listening on port %d', server.address().port);
+});
